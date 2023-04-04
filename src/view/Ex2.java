6a04eedf7a7b73ce2779051d1ba7b808fe6a9839
Mr_mu <Mr_mu@DESKTOP-R29GLLU>
2023-04-04 17:06:37 -0300

    Second Commit

--- src/view/Ex2.java
+++ src/view/Ex2.java
@@ -1,29 +1,37 @@
 package view;
 
-import javax.swing.JOptionPane;
-
 import br.edu.fateczl.pilhaint.Pilha;
-import controller.NPRController;
 
 public class Ex2 {
 
 	public static void main(String[] args) throws Exception {
 		
+		
 		Pilha pilha = new Pilha();
 		
-		NPRController npr = new NPRController();
 		
-		int valor;
+		int vetor[] = {0, 5, 7, -4, 3, 5, -2, -1, 10, 4, 3 , -6, 2, -9, 1, -5};
 		
-		String operador;
 		
-		for (int i = 0; i < 2; i++) {
-			valor = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor: "));
-			npr.insereValor(pilha, valor);
+		int v1 = 0;
+		int v2 = 0;
+		
+		
+		int somaPop;
+		
+		for(int i = 0; i < 16; i++) {
+			if(vetor[i] > -1) {
+				pilha.push(vetor[i]);
+			} else {
+				v1 = pilha.pop();
+				v2 = pilha.pop();
+				somaPop = v1 + v2;
+				pilha.push(v1);
+				pilha.push(somaPop);
+			}
 		}
 		
-		operador = JOptionPane.showInputDialog("Digite um operador: ");
-		System.out.println(npr.npr(pilha, operador));
+		System.out.println("Sua pilha contem "+pilha.size()+" valores.");
 
 	}
 
